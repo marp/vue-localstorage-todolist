0ff54a111483f5a02aeabc66555a66115e43be3e
user <you@example.com>
2020-09-27 21:06:23 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,90 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="newTask">
+      <input v-model="newTaskInput" placeholder="What you have to do?">
+      <button @click="addTask">Add Task</button>
+  </div>
+  <div v-for="(task, index) in tasks" :key="index">
+    <Task :task="task" :index="index" @remove="removeTask(index)" @done="doneTask(index)"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Task from "@/components/Task";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Task
+  },
+  data() {
+    return {
+      tasks: [],
+      newTaskInput: ''
+    }
+  },
+  mounted() {
+    if (localStorage.getItem('tasks')) {
+      this.loadTasks();
+    } else {
+      this.tasks = [
+          {content: 'Zakupy', done: false},
+          {content: 'Spacer z psem', done: true},
+          {content: 'Programowanie', done: false}
+        ]
+      this.saveTasks();
+    }
+  },
+  watch: {
+
+  },
+  methods: {
+    addTask(){
+      // JSON.parse(localStorage.tasks).tasks.unshift({content: 'xd'});
+      this.tasks.unshift({content: this.newTaskInput, done: false});
+      this.newTaskInput = '';
+      this.saveTasks();
+    },
+    removeTask(index){
+      this.tasks.splice(index, 1);
+      this.saveTasks();
+    },
+    doneTask(index){
+      this.tasks[index].done = !(this.tasks[index].done);
+      this.saveTasks();
+    },
+    saveTasks(){
+      localStorage.tasks = JSON.stringify(this.tasks);
+    },
+    loadTasks(){
+      this.tasks = JSON.parse(localStorage.tasks);
+    }
+
   }
+
 }
 </script>
 
 <style>
-#app {
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: lightgray;
+  width: 500px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+#newTask {
+  background-color: white;
+  border: 3px black double;
+  border-radius: 50px;
+}
+#newTask > input {
+  width: 80%;
 }
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>
+    <s v-if="task.done">{{ task.content }}</s>
+    <span v-else>{{ task.content }}</span>
+    <button @click="remove(index)">Remove</button>
+    <button @click="done(index)"><span v-if="!task.done">Done</span><span v-else>Undone</span></button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object,
+    index: Number
+  },
+  methods:{
+    remove(index){
+      this.$emit('remove', index);
+    },
+    done(index){
+      this.$emit('done', index);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
